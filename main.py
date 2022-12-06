016d1571331b440cfb3bf3fbf26d9be7df47709d
EndorphinSponge <hqiu1@ualberta.ca>
2022-12-05 22:14:08 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#%%
+import os
+import numpy as np
+import cv2
+from pytesseract import pytesseract
+
+#%% Constants
+pytesseract.tesseract_cmd = R"C:\Program Files\Tesseract-OCR\tesseract.exe" # Path to tesseract installation 
+
+#%%
+IMG_PATH = "data/ztest_consult1.png"
+IMG_PATH = "data/ztest_radiology1.png"
+IMG_ROOT = os.path.splitext(IMG_PATH)[0]
+image = cv2.imread(IMG_PATH)
+print(type(image))
+
+str_out = pytesseract.image_to_string(image)
+print(str_out)
+
+with open(F"{IMG_ROOT}.txt", "w+") as file:
+    file.write(str_out)
+# %%
